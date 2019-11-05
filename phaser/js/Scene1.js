185c0013602201f377bc95d0665df373154816cd
khalfani <kswing8@gmail.com>
2019-11-05 02:10:56 -0600

    Add person file. Edit Scene1.js to show elements

--- phaser/js/Scene1.js
+++ phaser/js/Scene1.js
@@ -1,4 +1,3 @@
-//Date Created [2019-11-02 Sat]
 //Purpose Makes scene that animates traversal of binary tree
 // Started 6:30
 
@@ -9,23 +8,48 @@ class Scene1 extends Phaser.Scene {
     }
     init(data) {}
     preload () {
-        // "../" means go up a directory
-        // the assets folder is in the directory above where this file is
-        //its called CAVE becuase its a constant
-        this.load.image("cave","/assets/images/cave.png");
+        //the directory is relative to the index.html file or whichever html
+        //file that Scene1 is loaded from
+        var imageDir = "/assets/images/";
+        this.load.image("cave",imageDir+"cave.png");
+        this.load.image("person",imageDir+"person.png");
     }
     create ()  {
-        this.CaveImage = this.add.image(400,300,"cave");
-        //move image
-        var movementRange = this.tweens.add(
-            {
-                targets: this.CaveImage,
-                x:200,
-                y:250,
-                delay:1000,
-                duration:2000
-            }
-        );
+        //Make list of images
+        var nodeList = [];
+
+        //This is hard coded
+        //TODO make this dynamic
+        var nodePositionArray = [
+                              [0,300],
+                              [100,300],
+                              [200,300],
+                              [300,300],
+                              [400,300],
+                              [500,300]
+                             ];
+        this.CaveImageArray = [];
+
+        //elements such as enemies or obstacles are stored as groups
+        https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Group.html
+        var caveGroup = this.add.group();
+        for (var i = 0; i < nodePositionArray.length; i++) {
+            caveGroup.create(
+                nodePositionArray[i][0],
+                nodePositionArray[i][1],
+                'cave');
+        }
+
+        // //move image
+        // var movementRange = this.tweens.add(
+        //     {
+        //         targets: this.CaveImage,
+        //         x:200,
+        //         y:250,
+        //         delay:1000,
+        //         duration:2000
+        //     }
+        // );
     }
     update(time, delta) {}
  
