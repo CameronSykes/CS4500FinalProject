1010ddc5f82dcd3b96c94c6e0b7f090db7e9353b
khalfani <kswing8@gmail.com>
2019-11-12 04:13:43 -0600

    Addedd Cave object to visualize animation and it works

---
+++ phaser3-game/src/scripts/objects/Cave.js
@@ -0,0 +1,33 @@
+
+/*A cave is a visual representation of a Node. It will be shown on the screen.
+
+We cannot intitlaize the base Node class in a BST without giving it coordinates
+and a scene. so this is a class that will let nodes be manipulated viusally*/
+
+export class Cave extends Phaser.GameObjects.Sprite {
+    
+    constructor(inputScene,
+                inputXCoord,
+                inputYCoord,
+                inputImageName,
+                // send node from BST
+                inputNode)
+
+    {
+        
+        //initalize Sprite class 
+        super(inputScene,
+              inputXCoord,
+              inputYCoord,
+              inputImageName);
+
+        // show the sprite on the scene
+        inputScene.add.existing(this);
+        // What could be diplayed in the tree
+        this.Node  = inputNode;
+
+    }
+    
+    
+    
+} 
