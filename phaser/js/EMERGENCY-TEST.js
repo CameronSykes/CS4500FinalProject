fde3caf957774e8bede13851b4fd1b11181dbc78
khalfani <kswing8@gmail.com>
2019-11-18 17:44:38 -0600

    Deleted unused files

--- phaser/js/EMERGENCY-TEST.js
+++
@@ -1,6 +0,0 @@
-import BinarySearchTree from "./BST.js";
-var myInstance = new BinarySearchTree();
-
-myInstance.root = 1;
-
-console.log(myInstance);
--- phaser/js/EXAMPLE-OF-IMPORT.js
+++
@@ -1,9 +0,0 @@
-//the from location is relative to the html file location
-
-//BST.js and This file are in the same location but since index.html is in a parent directory this file is located above
-import {BinarySearchTree, Node} from '/js/BST.js';
-
-var X = new BinarySearchTree();
-X.insert(3);
-X.insert(8);
-console.log(X.getNodesInOrder());
